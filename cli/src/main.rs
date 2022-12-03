373fcb8328079e0362f729f90b794dafb0745239
Caio Geraldes <caio.geraldes@usp.br>
2022-12-03 14:15:28 -0300

    rollback to clap 3.2.23

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,21 +1,29 @@
 use agldt::parser::*;
 use anyhow::Result;
-use clap::{Parser, Subcommand};
+use clap::*;
 use dotenv;
 use log;
 use pretty_env_logger;
 use std::collections::HashMap;
 use std::fs::{read_to_string, write};
 
+const HELP_TEMPLATE: &'static str = "\
+{before-help}{name} {version}
+{about-section}{author-section}
+{usage-heading} {usage}
+
+{all-args}{after-help}
+";
+
 #[derive(Parser, Debug)]
-#[command(author = "Caio Geraldes <caio.geraldes@usp.br>", version, about, long_about = None)]
-struct Args {
+#[clap(author, version, about, long_about = None, help_template = HELP_TEMPLATE)]
+struct Cli {
     /// AGLDT Treebank file
-    #[arg(value_name = "TREEBANK")]
+    #[clap(value_name = "TREEBANK")]
     treebank_file: String,
 
     /// Commands for running on the treebank
-    #[command(subcommand)]
+    #[clap(subcommand)]
     command: Option<Commands>,
 }
 
@@ -24,26 +32,32 @@ enum Commands {
     /// Builds a lexicon for the treebank
     Lexicon {
         /// File where to save the treebank
-        #[arg(short, long, default_value = "output.txt")]
+        #[clap(short, long, default_value = "output.txt")]
         output: String,
         /// Lists forms and not lemmata
-        #[arg(short, long, default_value = "false")]
+        #[clap(short, long, default_value = "false")]
         forms: bool,
         /// Counts entries
-        #[arg(short, long, default_value = "false")]
+        #[clap(short, long, default_value = "false")]
         count: bool,
     },
     /// Describes treebank
     Describe,
 }
 
-fn build_lemmata_lexicon_count(treebank: &Treebank, output: &str) -> Result<()> {
+fn build_lexicon_lemmata(treebank: &Treebank, output: &str, count: bool) -> Result<()> {
     let mut tokens: Vec<String> = vec![];
+    let tokens_string: String;
+
     for sentence in treebank.sentences() {
         for word in sentence.words() {
             if let Some(form_string) = word.lemma() {
                 if word.is_word() {
-                    tokens.push(form_string.into())
+                    if form_string.is_empty() {
+                        tokens.push(format!("UNKNOWN: {}", word.form()));
+                    } else {
+                        tokens.push(form_string.into());
+                    }
                 } else {
                     continue;
                 }
@@ -53,56 +67,71 @@ fn build_lemmata_lexicon_count(treebank: &Treebank, output: &str) -> Result<()> 
         }
     }
 
-    let mut m: HashMap<String, usize> = HashMap::new();
-    for x in tokens.iter() {
-        *m.entry(x.clone()).or_default() += 1;
-    }
+    if count {
+        log::info!("Writing list of lemmata with counts in {}", &output);
+        let mut m: HashMap<String, usize> = HashMap::new();
+        for x in tokens.iter() {
+            *m.entry(x.trim().to_string()).or_default() += 1;
+        }
 
-    let mut hash_vec: Vec<(&String, &usize)> = m.iter().collect();
-    hash_vec.sort_by(|a, b| b.1.cmp(a.1));
+        let mut hash_vec: Vec<(&String, &usize)> = m.iter().collect();
+        hash_vec.sort_by(|a, b| b.1.cmp(a.1));
 
-    let mut tokens: Vec<String> = vec![];
-    for i in hash_vec {
-        tokens.push(format!("\"{}\",{}", i.0, i.1));
+        let mut tokens: Vec<String> = vec![];
+        for i in hash_vec {
+            tokens.push(format!("\"{}\",{}", i.0, i.1));
+        }
+        tokens_string = tokens.join("\n");
+    } else {
+        log::info!("Writing list of lemmata in {}", &output);
+        for token in tokens.iter_mut() {
+            *token = token.trim().to_string();
+        }
+        tokens.sort();
+        tokens.dedup();
+        tokens_string = tokens.join("\n");
     }
-    let tokens_string = tokens.join("\n");
 
     write(output, tokens_string)?;
-
     Ok(())
 }
 
-fn build_form_lexicon(treebank: &Treebank, output: &str) -> Result<()> {
+fn build_lexicon_forms(treebank: &Treebank, output: &str, count: bool) -> Result<()> {
     let mut tokens: Vec<String> = vec![];
+    let tokens_string: String;
     for sentence in treebank.sentences() {
         for word in sentence.words() {
             let form_string = word.form().to_string();
-            if !tokens.contains(&form_string) & word.is_word() {
+            if word.is_word() {
                 tokens.push(form_string)
             }
         }
     }
-    tokens.sort();
-    let tokens_string = tokens.join("\n");
-    write(output, tokens_string)?;
-    Ok(())
-}
 
-fn build_lemmata_lexicon(treebank: &Treebank, output: &str) -> Result<()> {
-    let mut tokens: Vec<String> = vec![];
-    for sentence in treebank.sentences() {
-        for word in sentence.words() {
-            if let Some(form_string) = word.lemma() {
-                if !tokens.contains(&form_string.into()) & word.is_word() {
-                    tokens.push(form_string.into())
-                }
-            } else {
-                continue;
-            }
+    if count {
+        log::info!("Writing list of tokens with counts in {}", &output);
+        let mut m: HashMap<String, usize> = HashMap::new();
+        for x in tokens.iter() {
+            *m.entry(x.trim().to_string()).or_default() += 1;
         }
+
+        let mut hash_vec: Vec<(&String, &usize)> = m.iter().collect();
+        hash_vec.sort_by(|a, b| b.1.cmp(a.1));
+
+        let mut tokens: Vec<String> = vec![];
+        for i in hash_vec {
+            tokens.push(format!("\"{}\",{}", i.0, i.1));
+        }
+        tokens_string = tokens.join("\n");
+    } else {
+        log::info!("Writing list of tokens in {}", &output);
+        for token in tokens.iter_mut() {
+            *token = token.trim().to_string();
+        }
+        tokens.sort();
+        tokens.dedup();
+        tokens_string = tokens.join("\n");
     }
-    tokens.sort();
-    let tokens_string = tokens.join("\n");
     write(output, tokens_string)?;
     Ok(())
 }
@@ -116,8 +145,7 @@ fn main() -> anyhow::Result<()> {
     dotenv::dotenv().ok();
     pretty_env_logger::init();
 
-    let args = Args::parse();
-
+    let args = Cli::parse();
     let src: String;
 
     if let Ok(agldt_path) = dotenv::var("AGLDT_PATH") {
@@ -146,14 +174,9 @@ fn main() -> anyhow::Result<()> {
                 count,
             } => {
                 if forms {
-                    log::info!("Writing list of forms in {}", &output);
-                    build_form_lexicon(&src, &output)?;
-                } else if count {
-                    log::info!("Writing list of lemmata with counts in {}", &output);
-                    build_lemmata_lexicon_count(&src, &output)?;
+                    build_lexicon_forms(&src, &output, count)?;
                 } else {
-                    log::info!("Writing list of lemmata in {}", &output);
-                    build_lemmata_lexicon(&src, &output)?;
+                    build_lexicon_lemmata(&src, &output, count)?;
                 }
             }
             Commands::Describe => {
